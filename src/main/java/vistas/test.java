1e0919527444d02b9890ee31ad387543a7368233
Frankz <Frankz@PC-Frankz>
2023-01-08 23:29:41 -0500

    refactors

---
+++ src/main/java/vistas/test.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vistas;
+
+/**
+ *
+ * @author Frankz
+ */
+public class test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("=============================================");
+        System.out.println("-------SISTEMA GESTOR DE CITAS MEDICAS-------");
+        System.out.println("=============================================");
+        System.out.println("SELECCIONA TU TIPO DE USUARIO");
+        System.out.println("1.- Admin");
+        System.out.println("2.- Medico");
+    }
+    
+}
