8d5bb83cc4ba3a7ec8cad26431a7e281f453658b
Glender Miranda <Glender Miranda@DESKTOP-DIJMIJA>
2023-01-09 00:25:41 -0500

    Modificacion de VistaAdministrador

--- src/main/java/vistas/VistaAdministrador.java
+++ src/main/java/vistas/VistaAdministrador.java
@@ -13,11 +13,13 @@ import java.util.Scanner;
  * @author Frankz
  */
 public class VistaAdministrador {
+
     private Scanner sc;
+
     public VistaAdministrador() {
         sc = new Scanner(System.in);
     }
-    
+
     public void opcionesAdministrador() {
         System.out.println("-------BIENVENIDO AL SISTEMA-------");
         System.out.println("1. GESTION CITAS");
@@ -25,8 +27,8 @@ public class VistaAdministrador {
         System.out.println("3. GESTION PACIENTES");
         System.out.println("4. SALIR");
         int opcion = Integer.parseInt(sc.nextLine());
-        
-        switch(opcion) {
+
+        switch (opcion) {
             case 1:
                 administracionCitas();
                 break;
@@ -37,23 +39,25 @@ public class VistaAdministrador {
                 administracionPacientes();
                 break;
             default:
-                System.out.println("Saliendo del sistema...");                
+                System.out.println("Saliendo del sistema...");
         }
     }
-    
+
     private void administracionCitas() {
         VistaCitas vistaCitas = new VistaCitas();
+        VistaFactura vistaFactura = new VistaFactura();
         System.out.println("-------GESTION DE CITAS-------");
-        System.out.println("1. AGENDAR CITA");        
+        System.out.println("1. AGENDAR CITA");
         System.out.println("2. MOSTRAR CITAS");
         System.out.println("3. COMPLETAR CITA");
         System.out.println("4. MODIFICAR CITA");
         System.out.println("5. ELIMINAR CITA");
-        System.out.println("6. SALIR");
-        
+        System.out.println("6. FACTURAR CITA");
+        System.out.println("7. SALIR");
+
         int opcion = Integer.parseInt(sc.nextLine());
-        
-        switch(opcion) {
+
+        switch (opcion) {
             case 1:
                 vistaCitas.registrarCita();
                 break;
@@ -69,21 +73,24 @@ public class VistaAdministrador {
             case 5:
                 vistaCitas.eliminarCita();
                 break;
+            case 6:
+                vistaFactura.generarFactura();
+                break;
             default:
-                System.out.println("Saliendo del sistema...");                                                          
-        }        
+                System.out.println("Saliendo del sistema...");
+        }
     }
-    
-    private void administracionMedicos(){
+
+    private void administracionMedicos() {
         VistaMedico vistaMedico = new VistaMedico();
         System.out.println("-------GESTION DE MEDICOS-------");
         System.out.println("1. REGISTRAR MEDICO");
         System.out.println("2. VISUALIZAR CITAS PENDIENTES");
         System.out.println("3. VISUALIZAR CITAS COMPLETADAS");
         System.out.println("2. SALIR");
-        
+
         int opcion = Integer.parseInt(sc.nextLine());
-        switch(opcion) {
+        switch (opcion) {
             case 1:
                 vistaMedico.registrarMedico();
                 break;
@@ -98,18 +105,18 @@ public class VistaAdministrador {
                 vistaMedico.visualizarCitasCompletadas(cedulaMedico2);
                 break;
             default:
-                System.out.println("Saliendo del sistema...");                
+                System.out.println("Saliendo del sistema...");
         }
     }
-    
+
     private void administracionPacientes() {
         VistaPaciente vistaPaciente = new VistaPaciente();
         System.out.println("-------GESTION DE PACIENTES-------");
         System.out.println("1. REGISTRAR PACIENTE");
         System.out.println("2. SALIR");
         int opcion = Integer.parseInt(sc.nextLine());
-        
-        switch(opcion) {
+
+        switch (opcion) {
             case 1:
                 vistaPaciente.regristrarPaciente();
                 break;
@@ -117,6 +124,5 @@ public class VistaAdministrador {
                 System.out.println("Saliendo del sistema...");
         }
     }
-    
-    
+
 }
